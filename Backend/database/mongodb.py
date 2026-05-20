ee0f2bc6756d6177d75f7a49279196f0e62a29c1
kalpesh <kalpesh@kalpeshs-MacBook-Air.local>
2026-05-20 22:23:03 +0530

    Fixed MongoDB Atlas configuration

--- Backend/database/mongodb.py
+++ Backend/database/mongodb.py
@@ -1,8 +1,18 @@
 from pymongo import MongoClient
+import os
 
-client = MongoClient("mongodb://localhost:27017/")
+MONGO_URI = os.environ.get("mongodb+srv://GuardianAdmin:GNode@cluster0.ifqkmq1.mongodb.net/?appName=Cluster0")
+
+if not MONGO_URI:
+
+    raise Exception(
+        "MONGO_URI environment variable not set"
+    )
+
+client = MongoClient(MONGO_URI)
 
 db = client["guardiannode"]
 
 users_collection = db["users"]
+
 threats_collection = db["threats"]
